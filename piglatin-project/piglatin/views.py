ea87b8cede4628f94faf29aab5fa1420e89d5eac
Allan Liebold <allan.liebold@gmail.com>
2017-11-28 00:05:49 -0800

    wrote pig latin translator and html page with form to accept text.

---
+++ piglatin-project/piglatin/views.py
@@ -0,0 +1,33 @@
+"""Create page views for url routes."""
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    """View for home url."""
+    return render(request, 'home.html')
+
+
+def translate(request):
+    """View for translate url."""
+    input_text = request.GET['input_text'].lower()
+    translation = ''
+
+    for word in input_text.split():
+        if word[:2] in ('sh', 'th', 'tr', 'pl', 'gr', 'bl', 'cr', 'cl',
+                        'sl', 'ph', 'ch', 'dr', 'pr', 'br', 'fr'):
+            translation += word[2:]
+            translation += '-'
+            translation += word[:2]
+            translation += 'ay '
+        elif word[0] in ('a', 'e', 'i', 'o', 'u'):
+            translation += word
+            translation += '-yay '
+        else:
+            translation += word[1:]
+            translation += '-'
+            translation += word[0]
+            translation += 'ay '
+
+    return HttpResponse(translation)
