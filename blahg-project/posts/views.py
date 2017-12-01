7bc09c82573090c809a3a5bde6b2e7949329aad8
Allan Liebold <allan.liebold@gmail.com>
2017-12-01 11:50:30 -0800

    wrote homepage view for blog, displaying posts with title, image, publish date, and body.

--- blahg-project/posts/views.py
+++ blahg-project/posts/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+"""Posts views."""
+
+
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    """Home view."""
+    posts = Post.objects.order_by('pub_date')
+    return render(request, 'posts/home.html', {'posts': posts})
