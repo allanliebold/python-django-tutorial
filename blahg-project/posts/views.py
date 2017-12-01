f6e2b3ead3e6d629fa4851a4042509d2eb1dcd76
Allan Liebold <allan.liebold@gmail.com>
2017-12-01 13:37:50 -0800

    adds detail view for blog posts

--- blahg-project/posts/views.py
+++ blahg-project/posts/views.py
@@ -12,3 +12,8 @@ def home(request):
     """Home view."""
     posts = Post.objects.order_by('pub_date')
     return render(request, 'posts/home.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    """View for specific post."""
+    return render(request, 'posts/post_detail.html', {'post_id': post_id})
